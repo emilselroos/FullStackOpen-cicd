b82a24f2d78bc636c81617f25426ab151879a0ab
Emil Selroos <emil.selroos@gmail.com>
2021-12-30 09:14:21 +0200

    commit stuff

---
+++ src/services/persons.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+// const baseUrl = '/api/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const create = newContact => {
+  const request = axios.post(baseUrl, newContact)
+  return request.then(response => response.data)
+}
+
+const edit = (id, newContact) => {
+  const request = axios.put(`${baseUrl}/${id}`, newContact)
+  return request.then(response => response.data)
+}
+
+const remove = id => {
+  const request = axios.delete(`${baseUrl}/${id}`)
+  return request.then(response => response.data)
+}
+
+const personService = { getAll, create, edit, remove }
+
+export default personService
